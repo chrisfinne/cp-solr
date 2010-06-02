78dc2ebf3a451df4301872ecbde75bc30486eb0a
chrisfinne <chris.finne@gmail.com>
2010-06-02 14:56:50 -0700

    add vendored gems to rails app

--- rails_app/config/environment.rb
+++ rails_app/config/environment.rb
@@ -37,6 +37,7 @@ Rails::Initializer.run do |config|
 
 #  config.gem 'jdbc-mysql'
 #  config.gem 'jruby-openssl'
+#  config.gem 'rack'
   config.gem 'rsolr-direct', :version=>'0.1.0'
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
